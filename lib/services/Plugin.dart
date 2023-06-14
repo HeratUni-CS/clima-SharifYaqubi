b2eeb24abe4c7710cc9f790dfc91c26aea472084
Sharif <sharifyaqubi2023@gmail.com>
2023-06-14 15:25:46 +0430

    bring some changes inot home page

---
+++ lib/services/Plugin.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PlugIn {
+  PlugIn(this.url);
+
+  final String url;
+  // I plugIn to http to get the data
+  
+ // get datat from server
+  Future getData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
---
+++ lib/services/get_location.dart
@@ -0,0 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
+class GetLocation {
+  double lat;
+  double long;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.long);
+
+      lat = position.latitude;
+      long = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
